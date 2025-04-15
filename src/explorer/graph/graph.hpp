d8a2694312c67ebb4eb65417e4e65d9dc74da585
Ravenwater <theo@stillwater-sc.com>
2025-04-15 19:17:41 -0400

    WIP: trying to recover the erhe-sync and adding the domain-flow graph to the project

---
+++ src/explorer/graph/graph.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "erhe_graph/graph.hpp"
+
+namespace explorer {
+
+class Graph : public erhe::graph::Graph
+{
+public:
+    void evaluate();
+private:
+};
+
+} // namespace editor
