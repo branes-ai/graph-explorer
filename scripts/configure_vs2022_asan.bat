717ec2c7d33e36b366c0cc6a3e61716eb73eca5f
Timo Suoranta <tksuoran@gmail.com>
2025-04-26 20:09:41 +0300

    Update ASAN script

--- scripts/configure_vs2022_asan.bat
+++ scripts/configure_vs2022_asan.bat
@@ -2,21 +2,22 @@
 @rem      https://github.com/tksuoran/erhe/issues/139
 
 cmake ^
-    -G "Visual Studio 17 2022" ^
-    -A x64 ^
-    -B build ^
-    -S . ^
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ^
-    -Wno-dev ^
-    -DERHE_USE_PRECOMPILED_HEADERS=OFF ^
-    -DERHE_FONT_RASTERIZATION_LIBRARY=freetype ^
-    -DERHE_GLTF_LIBRARY=fastgltf ^
-    -DERHE_GUI_LIBRARY=imgui ^
-    -DERHE_PHYSICS_LIBRARY=jolt ^
-    -DERHE_PROFILE_LIBRARY=none ^
-    -DERHE_RAYTRACE_LIBRARY=bvh ^
-    -DERHE_SVG_LIBRARY=lunasvg ^
-    -DERHE_TEXT_LAYOUT_LIBRARY=harfbuzz ^
-    -DERHE_WINDOW_LIBRARY=sdl ^
-    -DERHE_XR_LIBRARY=openxr ^
-    -DERHE_USE_ASAN:BOOL=ON
+ -G "Visual Studio 17 2022" ^
+ -A x64 ^
+ -B build ^
+ -S . ^
+ --graphviz=erhe_cmake_dependencies.dot ^
+ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ^
+ -Wno-dev ^
+ -DERHE_USE_PRECOMPILED_HEADERS=OFF ^
+ -DERHE_FONT_RASTERIZATION_LIBRARY=freetype ^
+ -DERHE_GLTF_LIBRARY=none ^
+ -DERHE_GUI_LIBRARY=imgui ^
+ -DERHE_PHYSICS_LIBRARY=none ^
+ -DERHE_PROFILE_LIBRARY=none ^
+ -DERHE_RAYTRACE_LIBRARY=bvh ^
+ -DERHE_SVG_LIBRARY=lunasvg ^
+ -DERHE_TEXT_LAYOUT_LIBRARY=harfbuzz ^
+ -DERHE_WINDOW_LIBRARY=sdl ^
+ -DERHE_XR_LIBRARY=none ^
+ -DERHE_USE_ASAN:BOOL=ON
